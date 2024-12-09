477f4d05e71aebf246133346e80c82afaaeaedbe
inoveAlumnos <alumnos@inove.com.ar>
2024-12-08 21:03:45 -0300

    Ejercicios de practica

---
+++ ejercicios_practica/ejercicio_1.py
@@ -0,0 +1,31 @@
+# CODE:1
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+numero_1 = 5
+numero_2 = 7
+
+# Alumno:
+# Realizar la suma de las dos variables
+# Crear una variable llamada suma donde se 
+# almacene el valor de la suma de las variable numero_1 y numero_2
+# ej:
+# suma = .....
+
+# Imprimir en pantalla el resultado de la suma
+# print(....)
+
+# Repita el procedimiento para realizar la resta
+# Crear una variable llamada resta donde se 
+# almacene el valor de la resta de numero_1 menos numero_2
+
+
+# Imprimir en pantalla el resultado de la resta
---
+++ ejercicios_practica/ejercicio_4.py
@@ -0,0 +1,34 @@
+# CODE:4
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese tres palabras y arme un acrónimo con ellas
+palabra_1 = str(input('Ingrese palabra 1:'))
+
+palabra_2 = str(input('Ingrese palabra 2:'))
+
+palabra_3 = str(input('Ingrese palabra 3:'))
+
+# Objetivo:
+# De cada palabra ingresada se utilizará
+# la primera letra para armar un acrónimo, por ejemplo:
+# Si las palabras ingresadas fueran:
+# --> Alumbrado, barrido y limpieza
+# El acrónimo resultado es --> abl
+
+# Alumno:
+# Crear una variable llamada acronimo donde se 
+# almacene la primera letra de cada palabra
+# en el orden corespondiente
+
+
+
+# Imprimir la variable acronimo en pantalla
